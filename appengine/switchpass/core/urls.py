bf0c5b0acedb525c198bd7a35046435ca21ae6b4
Adam Alton <adamalton@gmail.com>
2015-12-18 17:37:17 +0000

    Building out the base template and home page a bit more.

--- appengine/switchpass/core/urls.py
+++ appengine/switchpass/core/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns(
     'switchpass.core.views',
     url(r'^$', 'home', name='home'),
+    url(r'^how-it-works/$', 'how_it_works', name='how_it_works'),
 )
--- appengine/switchpass/core/views.py
+++ appengine/switchpass/core/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home(request):
     """ Root of the site. """
     return render(request, "core/home.html", {})
+
+
+def how_it_works(request):
+    """ Page which explains how Switchpass works. """
+    return render(request, "core/how_it_works.html", {})
