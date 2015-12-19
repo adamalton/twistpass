1ebc62ec8a671f0fc74bca69ba80bdfdd6550ca4
Adam Alton <adamalton@gmail.com>
2015-12-19 12:39:03 +0000

    Starting to build out the 'generator' page.

--- appengine/switchpass/core/urls.py
+++ appengine/switchpass/core/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns(
     'switchpass.core.views',
     url(r'^$', 'home', name='home'),
+    url(r'^generator/$', 'generator', name='generator'),
     url(r'^how-it-works/$', 'how_it_works', name='how_it_works'),
 )
--- appengine/switchpass/core/views.py
+++ appengine/switchpass/core/views.py
@@ -6,6 +6,11 @@ def home(request):
     return render(request, "core/home.html", {})
 
 
+def generator(request):
+    """ The page with the actual password generator. """
+    return render(request, "core/generator.html", {})
+
+
 def how_it_works(request):
     """ Page which explains how Switchpass works. """
     return render(request, "core/how_it_works.html", {})
