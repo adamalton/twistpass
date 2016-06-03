af47558689101e5e9ac5046c8ecb19341bbcc7ba
Adam Alton <adamalton@gmail.com>
2016-06-03 12:01:02 +0100

    Adding `Cache-control:public` to views.

--- appengine/swappass/core/views.py
+++ appengine/swappass/core/views.py
@@ -2,52 +2,59 @@
 from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.decorators.cache import cache_page
+from django.views.decorators.cache import cache_page, cache_control
 
 
-def cache_page_bypass(*args, **kwargs):
-    """ No-op replacement for Django's @cache_page decorator. """
+def no_op_decorator(*args, **kwargs):
+    """ No-op replacement for any decorator. """
     def decorator(function):
         return function
     return decorator
 
 if settings.DEBUG:
-    cache_page = cache_page_bypass
+    cache_page = no_op_decorator
+    cache_control = no_op_decorator
 
 
 CACHE_TIME = 50 * 60
 
 
+@cache_control(public=True)
 @cache_page(CACHE_TIME)
 def home(request):
     """ Root of the site. """
     return render(request, "core/home.html", {})
 
 
+@cache_control(public=True)
 @cache_page(CACHE_TIME)
 def generator(request):
     """ The page with the actual password generator. """
     return render(request, "core/generator.html", {})
 
 
+@cache_control(public=True)
 @cache_page(CACHE_TIME)
 def how_it_works(request):
     """ Page which explains how SwapPass works. """
     return render(request, "core/how_it_works.html", {})
 
 
+@cache_control(public=True)
 @cache_page(CACHE_TIME)
 def feedback(request):
     """ Page that links to the Google Form where users can submit feedback. """
     return render(request, "core/feedback.html", {})
 
 
+@cache_control(public=True)
 @cache_page(CACHE_TIME)
 def tips(request):
     """ Tips for how to create a strong master password. """
     return render(request, "core/tips.html", {})
 
 
+@cache_control(public=True)
 @cache_page(CACHE_TIME)
 def learn(request):
     """ Root page for the articles section of the site. """
