a73368af1c54254b771bb37b84f1c938f2219388
Adam Alton <adamalton@gmail.com>
2020-02-24 17:04:12 +0000

    Add a `bin/deploy.py` script. Adapt app.yaml for deployment with gcloud.

---
+++ appengine/bin/deploy.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import subprocess
+import sys
+
+if sys.version.startswith("2."):
+    raise Exception(
+        "This script is written for python 3, not python 2. "
+        "It could easily be adapted (just the subprocess bit needs changing), "
+        "I just haven't done it yet."
+    )
+
+NEXT_VERSION = "9"
+
+print("Let's deploy project 'swappass-dot-com'...")
+promote = input("Route traffic to this deployment? [Y/n] ")
+promote = promote.lower() not in ("n", "no")
+version = input("What App Engine version do you want to give this deployment? [%s]" % NEXT_VERSION)
+version = version or NEXT_VERSION
+
+command = [
+    "gcloud",
+    "app",
+    "deploy",
+    "--project",
+    "swappass-dot-com",
+    "--version",
+    version,
+    "" if promote else "--no-promote",
+    "app.yaml"
+]
+process = subprocess.run(command)
